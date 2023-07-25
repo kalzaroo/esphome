2a84118e1de388d40b33f334d6f8fb0b2d6ad57f
hanpp0 <hanpp0@naver.com>
2023-07-25 20:32:42 +0900

    원복

--- esphome/components/rs485/sensor/rs485_sensor.h
+++ esphome/components/rs485/sensor/rs485_sensor.h
@@ -9,8 +9,7 @@ namespace rs485 {
 
 class RS485Sensor : public sensor::Sensor, public RS485Device {
   public:
-#    RS485Sensor() { device_name_ = &this->name_; }
-    RS485Sensor() { device_name_ = this->name_.to_string(); }
+    RS485Sensor() { device_name_ = &this->name_; }
     void dump_config() override;
     void publish(const uint8_t *data, const num_t len) override;
     bool publish(bool state) override { return false; }
